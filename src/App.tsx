80ef7c0ceec7235a5caa7505780a192f1a4e75c6
Ola4606 <olaoluwaadeyemo7@gmail.com>
2022-08-28 13:32:59 +0100

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,26 @@ import Nav from "./components/general/Nav";
 import LogoNav from "./components/general/LogoNav";
 import Home from "./components/home/Home";
 import BottomNav from "./components/general/BottomNav";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ScrollToTop from "./components/general/ScrollToTop";
 
 function App() {
   return (
     <div>
-      <Nav />
-      <div className="px-20">
-        <LogoNav />
-        <Home />
-      </div>
-      <BottomNav />
+      <Router>
+        <ScrollToTop />
+        <Routes>
+          <Nav />
+          <div className="px-20">
+            <LogoNav />
+            <Route path="/" element={<Home />} />
+            <Route path="/services" element={<Home />} />
+            <Route path="/giving" element={<Home />} />
+            {/* <Route path="*" element={<PageNotFound />} /> */}
+          </div>
+          <BottomNav />
+        </Routes>
+      </Router>
     </div>
   );
 }
