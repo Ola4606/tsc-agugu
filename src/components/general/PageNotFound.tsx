0b6c6788506221e3754831668e7116cbe96b8b64
Ola4606 <olaoluwaadeyemo7@gmail.com>
2022-08-30 11:52:15 +0100

    Fixed Page Not Found Page

--- src/components/general/PageNotFound.tsx
+++ src/components/general/PageNotFound.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function PageNotFound() {
   return (
-    <div>PageNotFound</div>
-  )
+    <div className="bg-white w-full flex flex-col justify-center items-center mt-16 md:mt-24">
+      <p className="font-bold font-title text-xl sm:text-2xl md:text-3xl lg:text-5xl text-black">
+        Page Not Found!
+      </p>
+      <Link to="/">
+        <div className="mt-4 md:mt-8 text-center bg-red-600 rounded-md py-1 px-2">
+          <p className="font-casual text-sm md:text-base text-white">
+            Home Page
+          </p>
+        </div>
+      </Link>
+    </div>
+  );
 }
 
-export default PageNotFound
+export default PageNotFound;
