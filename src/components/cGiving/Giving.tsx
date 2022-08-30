ee9a9706b04e4455c176ace273e58973ac8c2d66
Ola4606 <olaoluwaadeyemo7@gmail.com>
2022-08-30 10:43:00 +0100

    Made Giving Page Mobile Responsive

--- src/components/cGiving/Giving.tsx
+++ src/components/cGiving/Giving.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import GivingImage from "../../images/giving.png";
+import GivingImageSm from "../../images/giving-sm.png";
 
 function Giving() {
   return (
@@ -8,10 +9,13 @@ function Giving() {
         className="bg-center bg-no-repeat bg-[length:100%_auto] mt-10"
         style={{ backgroundImage: `url(${GivingImage})` }}
       >
-        <div className="bg-white/[.50] w-full h-[450px] flex justify-center items-center">
+        <div className="bg-white md:bg-white/[.50] w-full md:h-[450px] flex flex-col justify-center items-center">
           <p className="font-bold font-title text-xl sm:text-2xl md:text-3xl lg:text-5xl text-black">
             Giving
           </p>
+          <div className="xsm:max-w-[390px] sm:max-w-[550px] mt-12 md:hidden">
+            <img src={GivingImageSm} alt="" className="w-full h-auto rounded-xl" />
+          </div>
         </div>
       </div>
 
@@ -31,50 +35,34 @@ function Giving() {
         </p>
       </div>
 
-      <div className="mt-14 md:mt-24 text-center">
+      <div className="mt-14 md:mt-32 text-center">
         <p className="font-bold font-title text-lg leading-none text-start">
           FOR LOCAL (NIGERIAN) PAYMENTS:
         </p>
         <p className="font-casual text-base md:text-lg lg:text-xl text-start">
           Bank transfers can be made to this account
         </p>
-        <br />
-        <br />
-        <p className="font-bold font-title text-lg leading-none text-gray-600">
+        <p className="font-bold font-title text-lg leading-none text-gray-600 mt-4 md:mt-16">
           ECOBANK
         </p>
-        <br />
-        <p className="font-bold font-title text-lg leading-none text-gray-600">
+        <p className="font-bold font-title text-lg leading-none text-gray-600 mt-3 md:mt-8">
           THE STONE CHURCH AGUGU
         </p>
-        <br />
-        <br />
-        <p className="font-bold font-title text-xl sm:text-2xl md:text-3xl lg:text-5xl text-black underline underline-offset-4 decoration-slate-300">
+
+        <p className="font-bold font-title text-xl sm:text-2xl md:text-3xl lg:text-5xl text-black underline underline-offset-4 decoration-slate-300 mt-4 md:mt-16">
           0250063391
         </p>
-        <br />
-        <br />
-        <br />
-        <p className="font-casual text-base md:text-lg lg:text-xl">
+
+        <p className="font-casual text-base md:text-lg lg:text-xl mt-3 md:mt-8">
           Thank You for supporting the ministry!
         </p>
       </div>
 
-      <div className="mt-14 md:mt-24 text-center">
+      <div className="mt-14 md:mt-32 text-center">
         <p className="font-bold font-title text-lg leading-none text-start">
           FOR INTERNATION PAYMENTS:
         </p>
-        <br />
-        <br />
-        {/* <p className="font-bold font-title text-lg leading-none text-gray-600">
-          ECOBANK
-        </p>
-        <br />
-        <p className="font-bold font-title text-lg leading-none text-gray-600">
-          THE STONE CHURCH
-        </p>
-        <br /><br /> */}
-        <p className="font-bold font-title text-xl sm:text-2xl md:text-3xl lg:text-5xl text-black">
+        <p className="font-bold font-title text-xl sm:text-2xl md:text-3xl lg:text-5xl text-black mt-4 md:mt-16">
           Currently Unavailable
         </p>
       </div>
